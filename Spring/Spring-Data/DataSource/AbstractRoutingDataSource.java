f98a203802a32d0bb09df511f6878874bc3145e9
BluecatLee <jiaobujun@163.com>
2021-02-18 16:06:59 +0800

    datasource

---
+++ Spring/Spring-Data/DataSource/AbstractRoutingDataSource.java
@@ -0,0 +1,126 @@
+﻿package org.springframework.jdbc.datasource.lookup;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.jdbc.datasource.AbstractDataSource;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+
+// 抽象可路由数据源
+public abstract class AbstractRoutingDataSource extends AbstractDataSource implements InitializingBean {
+    @Nullable
+    private Map<Object, Object> targetDataSources;								// 保存目标数据源集合
+    @Nullable
+    private Object defaultTargetDataSource;										// 保存默认的数据源
+    private boolean lenientFallback = true;						
+    private DataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+    @Nullable
+    private Map<Object, DataSource> resolvedDataSources;
+    @Nullable
+    private DataSource resolvedDefaultDataSource;
+
+    public AbstractRoutingDataSource() {
+    }
+
+    public void setTargetDataSources(Map<Object, Object> targetDataSources) {
+        this.targetDataSources = targetDataSources;
+    }
+
+    public void setDefaultTargetDataSource(Object defaultTargetDataSource) {
+        this.defaultTargetDataSource = defaultTargetDataSource;
+    }
+
+    public void setLenientFallback(boolean lenientFallback) {
+        this.lenientFallback = lenientFallback;
+    }
+
+    public void setDataSourceLookup(@Nullable DataSourceLookup dataSourceLookup) {
+        this.dataSourceLookup = (DataSourceLookup)(dataSourceLookup != null?dataSourceLookup:new JndiDataSourceLookup());
+    }
+
+	// 初始化后的操作 就是解析目标数据源 存到resolvedDataSources
+    public void afterPropertiesSet() {
+        if(this.targetDataSources == null) {
+            throw new IllegalArgumentException("Property 'targetDataSources' is required");
+        } else {
+            this.resolvedDataSources = new HashMap(this.targetDataSources.size());
+            this.targetDataSources.forEach((key, value) -> {
+                Object lookupKey = this.resolveSpecifiedLookupKey(key);					// 解析路由键
+                DataSource dataSource = this.resolveSpecifiedDataSource(value);			// 解析数据源
+                this.resolvedDataSources.put(lookupKey, dataSource);
+            });
+            if(this.defaultTargetDataSource != null) {
+                this.resolvedDefaultDataSource = this.resolveSpecifiedDataSource(this.defaultTargetDataSource);
+            }
+
+        }
+    }
+
+	// 路由键 resolveSpecifiedLookupKey 子类可重写
+    protected Object resolveSpecifiedLookupKey(Object lookupKey) {
+        return lookupKey;
+    }
+
+	// 解析数据源的核心方法 resolveSpecifiedDataSource 子类可重写
+    protected DataSource resolveSpecifiedDataSource(Object dataSource) throws IllegalArgumentException {
+        if(dataSource instanceof DataSource) {
+            return (DataSource)dataSource;
+        } else if(dataSource instanceof String) {
+            return this.dataSourceLookup.getDataSource((String)dataSource);
+        } else {
+            throw new IllegalArgumentException("Illegal data source value - only [javax.sql.DataSource] and String supported: " + dataSource);
+        }
+    }
+
+    public Map<Object, DataSource> getResolvedDataSources() {
+        Assert.state(this.resolvedDataSources != null, "DataSources not resolved yet - call afterPropertiesSet");
+        return Collections.unmodifiableMap(this.resolvedDataSources);
+    }
+
+    @Nullable
+    public DataSource getResolvedDefaultDataSource() {
+        return this.resolvedDefaultDataSource;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return this.determineTargetDataSource().getConnection();
+    }
+
+    public Connection getConnection(String username, String password) throws SQLException {
+        return this.determineTargetDataSource().getConnection(username, password);
+    }
+
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return iface.isInstance(this)?this:this.determineTargetDataSource().unwrap(iface);
+    }
+
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return iface.isInstance(this) || this.determineTargetDataSource().isWrapperFor(iface);
+    }
+
+	// 决定数据源 子类可重写
+    protected DataSource determineTargetDataSource() {
+        Assert.notNull(this.resolvedDataSources, "DataSource router not initialized");
+        Object lookupKey = this.determineCurrentLookupKey();
+        DataSource dataSource = (DataSource)this.resolvedDataSources.get(lookupKey);
+        if(dataSource == null && (this.lenientFallback || lookupKey == null)) {
+            dataSource = this.resolvedDefaultDataSource;
+        }
+
+        if(dataSource == null) {
+            throw new IllegalStateException("Cannot determine target DataSource for lookup key [" + lookupKey + "]");
+        } else {
+            return dataSource;
+        }
+    }
+
+	// 决定路由键 子类实现
+    @Nullable
+    protected abstract Object determineCurrentLookupKey();
+}
---
+++ Spring/Spring-Data/DataSource/DataSourceBuilder.java
@@ -0,0 +1,114 @@
+﻿package org.springframework.boot.jdbc;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.boot.context.properties.bind.Bindable;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.boot.context.properties.source.ConfigurationPropertyName;
+import org.springframework.boot.context.properties.source.ConfigurationPropertyNameAliases;
+import org.springframework.boot.context.properties.source.ConfigurationPropertySource;
+import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
+import org.springframework.util.ClassUtils;
+
+public final class DataSourceBuilder<T extends DataSource> {
+    private static final String[] DATA_SOURCE_TYPE_NAMES = new String[]{"com.zaxxer.hikari.HikariDataSource", "org.apache.tomcat.jdbc.pool.DataSource", "org.apache.commons.dbcp2.BasicDataSource"};
+																// 如果没有指定数据源类型 默认依次查找的数据源
+	private Class<? extends DataSource> type;					// 数据源类型
+    private ClassLoader classLoader;
+    private Map<String, String> properties = new HashMap();		// 数据源属性 url driverClassName username password
+
+    public static DataSourceBuilder<?> create() {
+        return new DataSourceBuilder((ClassLoader)null);
+    }
+
+    public static DataSourceBuilder<?> create(ClassLoader classLoader) {
+        return new DataSourceBuilder(classLoader);
+    }
+
+    private DataSourceBuilder(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
+	// 构建数据源
+    public T build() {
+        Class<? extends DataSource> type = this.getType();
+        DataSource result = (DataSource)BeanUtils.instantiateClass(type);	// 初始化数据源对象
+        this.maybeGetDriverClassName();
+        this.bind(result);
+        return result;
+    }
+
+	// 如果没有driverClassName但是有url 则通过url解析出driverClassName
+    private void maybeGetDriverClassName() {
+        if(!this.properties.containsKey("driverClassName") && this.properties.containsKey("url")) {
+            String url = (String)this.properties.get("url");
+            String driverClass = DatabaseDriver.fromJdbcUrl(url).getDriverClassName();
+            this.properties.put("driverClassName", driverClass);
+        }
+
+    }
+
+	// 绑定资源
+    private void bind(DataSource result) {
+        ConfigurationPropertySource source = new MapConfigurationPropertySource(this.properties);
+        ConfigurationPropertyNameAliases aliases = new ConfigurationPropertyNameAliases();
+        aliases.addAliases("url", new String[]{"jdbc-url"});
+        aliases.addAliases("username", new String[]{"user"});
+        Binder binder = new Binder(new ConfigurationPropertySource[]{source.withAliases(aliases)});
+        binder.bind(ConfigurationPropertyName.EMPTY, Bindable.ofInstance(result));
+    }
+
+    public <D extends DataSource> DataSourceBuilder<D> type(Class<D> type) {
+        this.type = type;
+        return this;
+    }
+
+    public DataSourceBuilder<T> url(String url) {
+        this.properties.put("url", url);
+        return this;
+    }
+
+    public DataSourceBuilder<T> driverClassName(String driverClassName) {
+        this.properties.put("driverClassName", driverClassName);
+        return this;
+    }
+
+    public DataSourceBuilder<T> username(String username) {
+        this.properties.put("username", username);
+        return this;
+    }
+
+    public DataSourceBuilder<T> password(String password) {
+        this.properties.put("password", password);
+        return this;
+    }
+
+    public static Class<? extends DataSource> findType(ClassLoader classLoader) {
+        String[] var1 = DATA_SOURCE_TYPE_NAMES;
+        int var2 = var1.length;
+        int var3 = 0;
+
+        while(var3 < var2) {
+            String name = var1[var3];
+
+            try {
+                return ClassUtils.forName(name, classLoader);
+            } catch (Exception var6) {
+                ++var3;
+            }
+        }
+
+        return null;
+    }
+
+    private Class<? extends DataSource> getType() {
+        Class<? extends DataSource> type = this.type != null?this.type:findType(this.classLoader);
+        if(type != null) {
+            return type;
+        } else {
+            throw new IllegalStateException("No supported DataSource type found");
+        }
+    }
+}
